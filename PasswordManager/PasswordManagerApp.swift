981af4c5430d614b6e6e4997275163fc8d974bee
Darshan <darshankmm2@gmail.com>
2024-08-09 01:04:04 +0530

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Darshan on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
